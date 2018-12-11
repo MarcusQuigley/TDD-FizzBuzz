e2d5314666275832c23cd8df3768aaea37a9877a
[MarcusQ] <marcusquigley@gmail.com>
2018-12-11 11:56:42 -0500

    renaming and adding stack kata

---
+++ FizzBuzz.Test/StackKataTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.Test
+{
+    [TestClass]
+    public class StackKataTests
+    {
+       //implementing basic stack
+       //pop items off
+       //push items on and get lifo order
+       //if empty dont throw exception when you pop
+       //cant push invalid value onto stack
+
+    }
+}
