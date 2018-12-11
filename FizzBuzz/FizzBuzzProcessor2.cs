39a7484ccac61059f25bcdcc5d1f1905eaf1ee71
[MarcusQ] <marcusquigley@gmail.com>
2018-12-11 10:49:01 -0500

    init tests for FizzBuzz

---
+++ FizzBuzz/FizzBuzzProcessor2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzProcessor2 
+    {
+        public IEnumerable<string> Run()
+        {
+            var result= Enumerable.Range(1, 100)
+                              .Select(i =>  {
+                                  if (i % 3 == 0 && i % 5 == 0)
+                                      return "FizzBuzz";
+                                  if (i % 3 == 0)
+                                      return "Fizz";
+                                  else if (i % 5 == 0)
+                                      return "Buzz";
+                                  else
+                                      return i.ToString();
+
+                              })
+
+                             ;
+            return result;
+        }
+    }
+}
